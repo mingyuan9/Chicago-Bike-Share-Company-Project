a7dbe3a2f7fd7b30cbef5be4581478e1e80e8f37
Ming Yuan <126297838+mingyuan9@users.noreply.github.com>
2024-01-11 22:12:18 -0800

    Update Data_Wrangling.sql

--- Data_Wrangling.sql
+++ Data_Wrangling.sql
@@ -1,93 +1,98 @@
--- 1. first, I'm wondering is there any invalid data, for example, bike started_at is greater than or equal ended_date,
--- which means the user didn't even ride a bike or there's a system issue with datetime.
-
-SELECT COUNT(*)
-FROM
-(     
-    SELECT *
-    FROM bike_data
-    WHERE started_at >= ended_at
-) invalid_data
-
--- 2. We've got 382 invalid records from our dataset, which we could delete those records
-DELETE FROM bike_data
-WHERE started_at >= ended_at
-
--- 3. To check if we successfully delete invalid data, we could run the first step query again
-SELECT COUNT(*)
-FROM
-(     
-    SELECT *
-    FROM bike_data
-    WHERE started_at >= ended_at
-) invalid_data
-
--- The result above is 0, it seems we deleted invalid data successfully.
-
--- 4. Some other factors that I think is not useful for the analysis are bike duration too short or too long
--- for example, if bike duration is greater than 12 hour, it's probably because the user forgets to lock the bike 
-SELECT member_casual,
-       COUNT(*) AS users_w_unusual_ride
-FROM
-(SELECT id,
-       ride_id,
-       rideable_type,
-       started_at,
-       ended_at, 
-      (STRFTIME('%s',ended_at)-STRFTIME('%s',started_at))/60 as duration_mins,
-       member_casual
+--  1. First of all, I select total count of records in each month to double check if it's consistent with the count number in the excel sheet.
+SELECT STRFTIME('%m', started_at) as Month,
+                COUNT(*) as Total_count
 FROM bike_data
-WHERE duration_mins >= 720) unusual_ride -- greater than 12 hour ride
 GROUP BY 1
 
--- I found that 1911 casual users have unusual rides that more than 12 hrs, while only 499 users have abnormal rides.
--- I believe we should do data analysis exclude the data above. 
+-- Perfect! The result was exactly what we want and matched with our data in the excel.
 
--- 5. As for start_lat, start_lng, end_lat, end,lng are float data, I wanna make sure they don't have whitespace at the beginning or at the end
-SELECT start_lat,
-       TRIM(ROUND(start_lat,6)),
-       start_lng,
-       TRIM(ROUND(start_lng,6)),
-       end_lat,
-       TRIM(ROUND(end_lat,6)),
-       end_lng,
-       TRIM(ROUND(end_lng,6))
+/*2. I'm checking the validity  of data
+For example, if for some ride_ids, started_at time is greater than or equal to ended_at time, 
+it could happen because people changed their mind and don't want to bike any more, or could be the
+bike is not working so that people change to another bike, and then the data entry doesn't make sense and it's not valid.
+And we want to exclude these entries when analyzing data*/
+
+SELECT COUNT(*) as invalid_entries
+FROM
+(     
+    SELECT *
+    FROM bike_data
+    WHERE started_at >= ended_at
+) invalid_data
+
+-- So, we have 216 data entries that are invalid and should be exclude from our dataset.
+
+/*3. Other factors make data invalid could be ride length is abnormaly long.
+Usually the duration of a bike marathon range from 3 to 7 hours, so we exclude data if ride lenth is over 7 hours (420 mins), 
+ which could be someone forgot to lock the bike or could be system issues*/
+SELECT COUNT(*) AS ride_overtime_count
+FROM  
+(
+    SELECT 
+           DISTINCT ride_id,
+           (STRFTIME('%s',ended_at)-STRFTIME('%s',started_at))/60 as duration_mins
+           FROM bike_data
+           WHERE duration_mins >= 420
+) ride_overtime   --We found 4208 rides are invalid because it's barely possible that an ordinary person could ride over 7 hours without professional training.
+
+
+--4. I wanna explore more about data. How many types of bike? Any Null values there?
+SELECT DISTINCT rideable_type
 FROM bike_data
-LIMIT 10
 
--- 6.Updata dataset: update data of existing rows
-UPDATE bike_data
-SET start_lat = TRIM(ROUND(start_lat,6)),
-    start_lng = TRIM(ROUND(start_lng,6)),
-    end_lat = TRIM(ROUND(end_lat,6)),
-    end_lng = TRIM(ROUND(end_lng,6))
-    
+-- So we have 3 types: electric, classic, and docked bikes
 
--- 7. Create a data views that we could use for later analysis, only include rides under
-CREATE VIEW formatted_bike_data AS
+--5. How many types of membership?
+SELECT DISTINCT member_casual
+FROM bike_data
+
+-- Cool, we only have 2 types of users: member, and casual
+
+-- 6. So here, I wanna create a new view that exclude invalid data (abnormal ride length and invalid started_at time)
+SELECT COUNT(*) as invalid_count  -- 4424 invalid data, which is consistent with previous result (4208 + 216)
+FROM
+        (
 SELECT id,
-       ride_id,
-       rideable_type,
-       started_at,
-       ended_at,
-       CASE cast(STRFTIME('%w',started_at) as interger) 
-            WHEN 0 THEN 'Sun'
-            WHEN 1 THEN 'Mon'
-            WHEN 2 THEN 'Tues'
-            WHEN 3 THEN 'Wed'
-            WHEN 4 THEN 'Thur'
-            WHEN 5 THEN 'Fri'
-            ELSE 'Saturday' END AS Day_of_Week,  -- new column
-       (STRFTIME('%s',ended_at)-STRFTIME('%s',started_at))/60 as duration_mins,
-       start_station_name,
-       start_station_id,
-       end_station_name,
-       end_station_id,
-       start_lat,
-       start_lng,
-       end_lat,
-       end_lng,
-       member_casual
+               ride_id,
+               rideable_type,
+               started_at,
+               ended_at,
+               start_station_name,
+               start_station_id,
+               end_station_name,
+               end_station_id,
+               start_lat,
+               start_lng,
+               end_lat,
+               end_lng,
+               member_casual
 FROM bike_data
-WHERE duration_mins < 720
+WHERE started_at >= ended_at
+              OR (STRFTIME('%s',ended_at)-STRFTIME('%s',started_at))/60 >= 420
+              ) invalid_data
 
+-- Create a view: formatted_bike_data       
+CREATE VIEW formatted_bike_data AS
+SELECT *
+FROM
+        (
+SELECT id,
+               ride_id,
+               rideable_type,
+               started_at,
+               ended_at,
+               start_station_name,
+               start_station_id,
+               end_station_name,
+               end_station_id,
+               start_lat,
+               start_lng,
+               end_lat,
+               end_lng,
+               member_casual
+FROM bike_data
+WHERE started_at < ended_at 
+                AND (STRFTIME('%s',ended_at)-STRFTIME('%s',started_at))/60 < 420
+              ) valid_data
+
+-- Our later on analysis would based on formatted  dataset.
